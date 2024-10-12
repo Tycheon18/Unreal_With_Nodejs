665bedc91175bd434975defefec44ec7893c80e3
Tycheon18 <joong0818@gmail.com>
2024-10-12 14:42:34 +0900

    Create new logic to post

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require("express");
 const app = express();
 
 const nunjucks = require("nunjucks");
+const bodyparser = require("body-parser")
 
 app.set("view engine", "html")
 nunjucks.configure("./views", {
     express: app
 })
+
+app.use(bodyparser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     console.log(req.query)
@@ -25,7 +28,10 @@ app.get("/nodejs", (req, res) => {
 const port = 3000;
 
 app.post("/nodejs", (req, res) => {
-    res.send("<h1>post 방식의 요청입니다.</h1>");
+    console.log(req.body)
+
+    const Color = req.body.Color;
+    res.send(`<h1 style="color:${Color};">post 방식의 요청입니다.</h1>`);
 });
 
 app.listen(port, () => {
